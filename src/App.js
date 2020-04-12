83ac6a3280235635787fd52d7a9643ce7ac3e3f2
burn-c <chopcsom@gmail.com>
2020-04-12 02:54:42 -0300

    Feature: Challenge 03

--- src/App.js
+++ src/App.js
@@ -1,30 +1,102 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  useEffect(()  =>  {
+    listRepositories();
+  },[]);
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title,
+      url,
+      techs,
+    })
+
+    if (response.status === 200) {
+      setRepositories([...repositories, response.data]);
+      setTitle('');
+      setUrl('');
+      setTechs('');
+    }
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    
+    if (response.status === 204) {
+
+      const newList = [...repositories];
+      const index = newList.filter(repo => repo.id !== id);
+
+      newList.splice(index,1);
+
+      setRepositories(newList);
+
+    } 
+
   }
 
+  async function listRepositories() {
+    const response = await api.get('repositories');
+
+    setRepositories(response.data);
+  }
+
+  
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+       {repositories.length > 0 
+       ?
+       repositories.map(repository => (         
+        <li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+          Remover
           </button>
         </li>
+      ))
+    : ''
+    }
       </ul>
+      
+    <label htmlFor="url">URL</label>
+    <input 
+    placeholder="URL"
+    value={url}
+    onChange={e => setUrl(e.target.value)}
+    type="text"
+    />
+
+    <label htmlFor="title">Title</label>
+    <input 
+    placeholder="Title"
+    value={title}
+    onChange={e => setTitle(e.target.value)}
+    type="text"
+    />
+
+    <label htmlFor="Techs">Techs</label>
+    
+    <input 
+    placeholder="Techs"
+    value={techs}
+    onChange={e => setTechs(e.target.value.split(','))}
+    type="text"
+    />
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+     </div>
   );
 }
 
